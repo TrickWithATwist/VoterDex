b8a7e76e828aa4f7af981d0725cc15d425102f03
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2024-09-28 15:15:31 -0400

    wip commit entry page

---
+++ frontend/voterdex/src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import './Entry.css';
+
+
+export default function Entry()
+{
+    return(
+        <body>
+            <div className='flexbox'>
+                <div className='header'>
+                    <div className='logo'>
+
+                    </div>
+                </div>
+            </div>
+        </body>
+    )
+}
